ff7419243d9a5370217124a39db9bad9203ce13e
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-07-12 16:44:59 +0500

    first

--- src/pages/Home/Blend.jsx
+++ src/pages/Home/Blend.jsx
@@ -3,26 +3,23 @@ import React from "react";
 import cloud from "../../assets/images/cloud.png";
 import sharp from "../../assets/images/sharpe.png";
 import dollar from "../../assets/images/dollar.png";
-import title1 from "../../assets/title1.svg";
-import title2 from "../../assets/title2.svg";
-import title3 from "../../assets/title3.svg";
 import bg from "../../assets/bg.svg";
 const Blend = () => {
   const faqData = [
     {
-      title: title1,
+      title: "Carbon Neutral",
       text: `We offset all the emissions caused by manufacturing, logistics, and digital operations through Carbon Capture. Thanks to this technology, CO2 is absorbed directly from the atmosphere, providing a much quicker compensation compared to tree planting.`,
       className: "cloud_img",
       img: cloud,
     },
     {
-      title: title2,
+      title: "Good Materials",
       text: `We prioritized materials with eco-friendly characteristics such as recycled polyester for the strap and biodegradable TPU for the cases (the clear case  also features PC). For the packaging we only used simple and recyclable paper, without any plastic.`,
       className: "sharp_img",
       img: sharp,
     },
     {
-      title: title3,
+      title: "MarketPlace",
       text: `Tired of your Fasten Case? Let somebody else give it some love and save resources. We are working on a marketplace where it will be easy for you to find a buyer among our visitors. Coming soon!`,
       className: "dollar_img",
       img: dollar,
@@ -37,7 +34,7 @@ const Blend = () => {
             return (
               <div key={i} className="blend_content">
                 <div className="active_faq">
-                  <img className="title" src={d.title} />
+                  <h1 className="titleFont">{d.title}</h1>
                   <p>{d.text}</p>
                 </div>
                 <div className="blend_imgs">
