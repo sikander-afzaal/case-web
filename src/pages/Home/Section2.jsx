423286bcd2ce20f8a49109ca64b56b2fdb6cd48f
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-07-14 16:09:14 +0500

    final

---
+++ src/pages/Home/Section2.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import img1 from "../../assets/img1.png";
+import img2 from "../../assets/img2.png";
+function Section2() {
+  return (
+    <div className="section2">
+      <h1>Fasten Your Phone</h1>
+      <img src={img1} alt="" />
+      <img src={img2} alt="" />
+    </div>
+  );
+}
+
+export default Section2;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,14 +4,16 @@ import Hero from "./Hero";
 import LaunchEdition from "./LaunchEdition";
 import Blend from "./Blend";
 import Footer from "../../layout/Footer";
-import Package from "./Package";
+// import Package from "./Package";
+import Section2 from "./Section2";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <Section2 />
       <LaunchEdition />
-      <Package />
+      {/* <Package /> */}
       <Blend />
       <Footer />
     </>
